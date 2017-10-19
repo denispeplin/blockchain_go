41498ab2cba7aadda9d9ed1436cad00772cecfcf
denispeplin <denis.peplin@gmail.com>
2017-10-19 22:02:07 +0300

    Use a function for standard error handling.

--- utils.go
+++ utils.go
@@ -6,12 +6,16 @@ import (
 	"log"
 )
 
-func IntToHex(num int64) []byte {
-	buff := new(bytes.Buffer)
-	err := binary.Write(buff, binary.BigEndian, num)
+func RaiseError(err error) {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	RaiseError(err)
 
 	return buff.Bytes()
 }
